9d14e3327e3618dc88698ab51ad73b0a24d9a81e
Jim124 <fengpingan065@gmail.com>
2024-08-18 19:35:28 +0800

    modify userSlice

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,12 +1,45 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
-  name: 'user slice',
+import { toast } from '@/components/ui/use-toast';
+
+export type User = {
+  username: string;
+  jwt: string;
 };
+
+type UserSlice = {
+  user: User | null;
+};
+
+const getLocalStorageUser = (): User | null => {
+  const user = localStorage.getItem('user');
+  return user ? JSON.parse(user) : null;
+};
+
+const initialState: UserSlice = {
+  user: getLocalStorageUser(),
+};
+
 const userSlice = createSlice({
-  name: 'cart',
+  name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    loginUser: (state, action: PayloadAction<User>) => {
+      const user = action.payload;
+      state.user = user;
+      localStorage.setItem('user', JSON.stringify(user));
+      if (user.username === 'demo user') {
+        toast({ description: 'Welcome Guest User' });
+        return;
+      }
+      toast({ description: 'Login successful ' });
+    },
+    logoutUser: (state) => {
+      state.user = null;
+      // localStorage.clear()
+      localStorage.removeItem('user');
+    },
+  },
 });
 
 export default userSlice.reducer;
