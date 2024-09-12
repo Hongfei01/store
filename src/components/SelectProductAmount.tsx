892b4039458ca98dfecfbd3b1a5df5085a41a1bc
Jim124 <fengpingan065@gmail.com>
2024-09-12 20:40:24 +0100

    modify SelectProductAmount component

--- src/components/SelectProductAmount.tsx
+++ src/components/SelectProductAmount.tsx
@@ -32,7 +32,7 @@ function SelectProductAmount({
 
   return (
     <>
-      <h4 className='mb-2 capitalize tracking-wider font-medium '>amount:</h4>
+      <h4 className='mb-2 capitalize tracking-wider font-medium'>amount:</h4>
       <Select
         defaultValue={amount.toString()}
         onValueChange={(value) => setAmount(Number(value))}
